8d253fc2a0e4bc52ae9426aaa9e4552ffca2e7f1
staraneelpatra <staraneelpatra@github.com>
2018-04-02 10:29:28 +0530

    route config

--- employeeManagement/src/app/app.module.ts
+++ employeeManagement/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'employees', component: EmployeesComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EmployeesComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
